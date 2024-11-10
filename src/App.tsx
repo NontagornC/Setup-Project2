396a456c0f2252e6bc93ad73a84283c36d113cbf
Nontagorn <nontagorn.ch@gmail.com>
2024-11-10 23:53:54 +0700

    add alias path

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
+import Test from "@/component/test";
+
 function App() {
-  return <div className="text-red-700">hello</div>;
+  return (
+    <div className="text-red-700">
+      <Test />
+    </div>
+  );
 }
 
 export default App;
